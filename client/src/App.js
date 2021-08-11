764d39660964a395b5c57d9f8cc77604093e64d8
Steven Schaab <stevenes83@gmail.com>
2021-08-11 15:57:37 -0500

    mock blog login to post

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
 
-const user = "blehbleh"
-const posts = [
+const defaultPosts = [
   {title:"React Hooks", content:"The greatest thing since sliced bread", author:"blehbleh"},
   {title:"React Moops", content:"The worst thing since sliced bread",author:"blehbleah"}
 ]
 
 export default function App () {
+  const [ user, setUser ] = useState('')
+  const [ posts, setPosts ] = useState(defaultPosts)
+
   return (
     <div style={{ padding: 8 }}>
-      <UserBar />
+      <UserBar user={user} setUser={setUser} />
       <br />
-      <CreatePost user={user} />
+      {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
