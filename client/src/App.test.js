96c8ebea5b48be570f91e992d30b202778a67df6
Steven Schaab <stevenes83@gmail.com>
2021-09-05 12:33:39 -0500

    added eslint/skipping getting auto fix working for the moment

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { RequestProvider } from 'react-request-hook'
+import axios from 'axios'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000/api/'
+})
+
+// "mock" window.scroll function to avoid errors in test runs
+Object.defineProperty(global.window, 'scroll', { value: function () {} })
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <RequestProvider value={axiosInstance}>
+      <App />
+    </RequestProvider>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
--- client/src/post/CreatePost.js
+++ client/src/post/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import {useResource} from 'react-request-hook'
 import {useNavigation} from 'react-navi'
 import {useInput} from 'react-hookedup'
