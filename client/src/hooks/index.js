37c4a1a103d225acf214eb1caab2ac41715dbb62
Steven Schaab <stevenes83@gmail.com>
2021-09-05 15:54:15 -0500

    added 6 custom hooks

---
+++ client/src/hooks/index.js
@@ -0,0 +1,8 @@
+import useTheme from './useTheme'
+import useDispatch from './useDispatch'
+import usePostsState from './usePostsState'
+import useUserState from './useUserState'
+import useDebouncedState from './useDebouncedState'
+
+export {useTheme, useDispatch, usePostsState, useUserState, useDebouncedState}
+export * from './api'
